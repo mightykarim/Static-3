72bf34bd85da6cc8aa7b166be369d1d40be02bd2
Muhammad <i221612@nu.edu.pk>
2025-11-19 22:36:14 +0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Codacy Java Project!");
+        int result = addNumbers(10, 20);
+        System.out.println("Sum: " + result);
+    }
+
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
